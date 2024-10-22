35646ad1056db937d46b55bdea1db1cc188e6ccd
Faraz <161611764+FarazKhoso@users.noreply.github.com>
2024-10-22 14:12:24 -0700

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Header from "./header/header";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+
+      {/* Hero section */}
+      <section className="bg-gray-900 text-white py-20">
+        <div className="container mx-auto flex justify-between items-center">
+          {/* Hero text */}
+          <div>
+            <h1 className="text-5xl font-bold">IPhone 14 Pro</h1>
+            <p className="mt-4 text-lg">
+              Created to change everything for the better. For everyone.
+            </p>
+            <button className="mt-6 px-6 py-2 bg-blue-600 rounded-md">
+              Shop Now
+            </button>
+          </div>
+          {/* Hero image */}
+          <div className="relative w-1/2">
+
+            <img
+              src="/images/Iphone Image.jpg"
+              alt=""
+              className="max-h-[400px] max-w-[400]"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
